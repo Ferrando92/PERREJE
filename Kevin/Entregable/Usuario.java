a3bb05fc9b0f5262d90c051d9edc8d44d3de04e1
Kevin Zarzycki <kezarszy@inf.upv.es>
2015-05-20 18:44:41 +0200

    Update Usuario.java

--- Kevin/Entregable/Usuario.java
+++ Kevin/Entregable/Usuario.java
@@ -16,8 +16,22 @@ public class Usuario {
 			throw new UserNotFoundException("El usuario no existe");
 	}
 	
-	public void crearNuevoPersonaje(){
-		this.personajes[numPersonajes] = new Personaje();
+	public void crearNuevoPersonaje(String nombre, String raza, int nivel){
+		this.personajes[numPersonajes] = new Personaje(nombre,raza,nivel);
 		this.numPersonajes++;
+	}
+	
+	public void crearNuevoPersonaje(String nombre, String raza){
+		this.personajes[numPersonajes] = new Personaje(nombre,raza,1);
+		this.numPersonajes++;
+	}
+	
+	public void borrarPersonaje(String nombre){
+		for(int i=0; i<personajes.length; i++){
+			if(personajes[i].nombre.equals(nombre)){
+				personajes[i] = null;
+				break;
+			}
+		}
 	}
 }
