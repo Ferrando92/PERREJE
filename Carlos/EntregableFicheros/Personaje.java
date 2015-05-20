56dc7d2b42107efb459a76effad810652890a0a5
Carlos Mocholí <Carlossmocholi@hotmail.com>
2015-05-20 18:33:48 +0200

    Create Personaje.java

---
+++ Carlos/EntregableFicheros/Personaje.java
@@ -0,0 +1,11 @@
+public class Personaje
+{
+    public String nombre, raza;
+    public int nivel;
+    
+    public Personaje(String nombre, String raza, int nivel){
+        this.nombre = nombre;
+        this.raza = raza;
+        this.nivel = nivel;
+    }
+}
